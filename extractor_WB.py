7dfcf066d167f882b72d057c20d5fd2aa49d5b2a
Alex13101 <podiyachiy-a@mail.ru>
2023-05-16 18:50:19 +0300

    Обернул в функцию

--- extractor_WB.py
+++ extractor_WB.py
@@ -11,26 +11,35 @@ def get_products(url):
 
 search = input('Что будем искать? ')
 
-urls = [f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&curr=rub&dest=-1281648&page={i}&query={search}&regions=80,64,38,4,115,83,33,68,70,69,30,86,75,40,1,66,48,110,31,22,71,114&resultset=catalog&sort=popular&spp=22&suppressSpellcheck=false'
-        for i in range(1, 61)
-]
-all_products =[]
-current_date = date.today()
-current_date_time = datetime.datetime.now()
-current_time = current_date_time.time()
-start = time.time()
-for url in urls:
-    all_products += get_products(url)
 
-print(time.time() - start)
-print(len(all_products))
-print(len(all_products[1]))
+
+
+start = time.time()
+def search_right_product(search):
+    products = []
+    urls = [
+        f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&curr=rub&dest=-1281648&page={i}&query={search}&regions=80,64,38,4,115,83,33,68,70,69,30,86,75,40,1,66,48,110,31,22,71,114&resultset=catalog&sort=popular&spp=22&suppressSpellcheck=false'
+        for i in range(1, 61)]
+    for url in urls:
+        products += get_products(url)
+
+    return products
+
+
+# print(len(all_products))
+# print(len(all_products[1]))
+
+
+long_lst = len(search_right_product(search))
+all_products = search_right_product(search)
 
 excel_file = openpyxl.load_workbook('Base.xlsx')  # Открытие exel-файла
 sheet_sheet = excel_file['Лист1']
+current_date = date.today()
+current_date_time = datetime.datetime.now()
+current_time = current_date_time.time()
 
-for i in range(0, len(all_products)):
-
+for i in range(0, long_lst):
     title = all_products[i].setdefault("name")
     price = all_products[i].setdefault('salePriceU')
     id_product = all_products[i].setdefault('id')
@@ -43,9 +52,12 @@ for i in range(0, len(all_products)):
     sheet_sheet.cell(row=i + 3, column=5).value = price/100
     sheet_sheet.cell(row=i + 3, column=6).value = current_date
     sheet_sheet.cell(row=i + 3, column=7).value = current_time
-    print( 'Готово!')
+    #print( 'Готово!')
 excel_file.save('Base.xlsx')
 
 excel_file.close()
+
+print(time.time() - start)
+
 # for k, v in all_products[15].items():
 #     print(k, v)
